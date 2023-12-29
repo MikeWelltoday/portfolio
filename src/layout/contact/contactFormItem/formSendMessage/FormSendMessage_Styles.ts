58268a51719f50a116e1d3996fbf53b14a7ffc3f
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2023-12-29 20:20:16 +0300

    => saved

--- src/layout/contact/contactFormItem/formSendMessage/FormSendMessage_Styles.ts
+++ src/layout/contact/contactFormItem/formSendMessage/FormSendMessage_Styles.ts
@@ -1,4 +1,4 @@
-import styled, {css} from 'styled-components'
+import styled from 'styled-components'
 import {ButtonFrame} from '../../../../components/ButtonFrame'
 import {motion} from 'framer-motion'
 import {ThemeSet} from '../../../../styles/ThemeStyles.styled'
